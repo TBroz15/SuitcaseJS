c764bafd9b6153c3bbb6fc57ecd1eb3985c260d0
TBroz15 <78770366+TBroz15@users.noreply.github.com>
2024-08-03 09:07:32 +0800

    build: comply with .d.ts and non-ts files

--- build.js
+++ build.js
@@ -1,14 +1,60 @@
 import esbuild from "esbuild";
-import { resolve } from "path";
+import { basename, extname, join } from "path";
 import { fdir } from "fdir";
+import { copyFile, mkdir } from "fs/promises";
 
-esbuild
+const src = "src";
+const dist = "dist";
+
+const directories = [];
+const TSFiles = [];
+const etc = [];
+
+const arr = await new fdir().withFullPaths().crawl(src).withPromise();
+
+arr.shift();
+
+const regexExt = /\.([^.]+)$/;
+
+arr.forEach((path) => {
+  let ext = extname(path);
+  if (ext === "") {
+    const realExt = regexExt.exec(basename(path));
+    if (realExt === null) return directories.push(path);
+    ext = realExt[0];
+  }
+
+  if (ext === ".ts") {
+    const splitPath = path.split(".");
+
+    // if file is a suspected .d.ts
+    if (splitPath[1] === "d") return etc.push(path);
+
+    return TSFiles.push(path);
+  } else {
+    return etc.push(path);
+  }
+});
+
+const directoryPromises = directories.map((path) =>
+  mkdir(join(src, path), { recursive: true })
+);
+
+await Promise.all(directoryPromises);
+
+const etcPromises = etc.map((path) =>
+  copyFile(join(src, path), join(dist, path))
+);
+
+await Promise.all(etcPromises);
+
+await esbuild
   .build({
-    entryPoints: await new fdir().withFullPaths().crawl("./src/").withPromise(),
+    entryPoints: TSFiles,
 
     platform: "node",
     target: "node18",
-    outdir: resolve("./dist/"),
+    outdir: dist,
   })
   .then(() => {
     console.log("Build complete.");
