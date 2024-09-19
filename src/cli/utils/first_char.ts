ce9b76f084880e3f790af4b16e0f6700d415ea6f
TBroz15 <78770366+TBroz15@users.noreply.github.com>
2024-09-19 18:08:03 +0800

    refactor(cli): :coffin: remove dead code

--- src/cli/utils/first_char.ts
+++
@@ -1,5 +0,0 @@
-export const upFirstChar = (string: string) =>
-  string.charAt(0).toUpperCase() + string.slice(1);
-
-export const lowFirstChar = (string: string) =>
-  string.charAt(0).toLowerCase() + string.slice(1);
