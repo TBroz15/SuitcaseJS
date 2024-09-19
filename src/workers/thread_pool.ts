35c92771c411450a693c35d9b1ca65ab9b0dd452
TBroz15 <78770366+TBroz15@users.noreply.github.com>
2024-09-19 16:38:07 +0800

    perf(compiler@other): :zap: full multithreading rewrite!
    This change managed to cut off 1.5 seconds of compilation in bedrock-samples pack 1.21.20 (including res and behavior.) The main thread have to send once at each thread at the start instead of posting messages to the workers mulitple times. Workers will automatically be terminated once all tasks are done. Workers won't have to copy non-compilable files and let the main thread do the work.

--- src/workers/thread_pool.ts
+++ src/workers/thread_pool.ts
@@ -1,90 +1,49 @@
+import { resolve } from "path";
 import { Worker } from "node:worker_threads";
-import { cache, tempPack } from "../utils/compiler/temp_folder.js";
-import { newSpinner } from "../utils/cli/spinner.js";
-import { lowFirstChar, upFirstChar } from "../utils/cli/first_char.js";
-import { resolve, dirname } from "node:path";
-import { fileURLToPath } from "url";
-import { FunctionNames } from "../types/workers.js";
-import type { CompilerConf } from "../types/config.d.ts";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import type { Options } from "../types/config.js";
 
-interface Thread {
-  id: number;
-  worker: Worker;
-  busy: boolean;
-}
+// interface ThreadError {
+//   error: string;
+//   filePath: string;
+// }
 
-export const ThreadPool = (threads: number, compilerConfig: CompilerConf) => {
-  const encoder = new TextEncoder();
+export class ThreadPool {
+  protected promises: Promise<unknown>[] = [];
 
-  const threadPool: Thread[] = [];
+  constructor(
+    threads: number,
+    inPath: string,
+    files: { JSONFiles: unknown[][]; PNGFiles: unknown[][] },
+    compilerConfig: Options
+  ) {
+    const workerFile = resolve(import.meta.dirname, "worker.js");
 
-  for (let i = 0; i < threads; i++) {
-    const worker = new Worker(resolve(__dirname, "worker.js"), {
-      workerData: {
-        compilerConfig,
-        temp: tempPack,
-        cache: cache,
-      },
-    });
-    threadPool.push({ id: i, worker, busy: false });
+    for (let i = 0; i < threads; i++) {
+      const worker = new Worker(workerFile, {
+        workerData: {
+          inPath,
+          files: {
+            JSON: files.JSONFiles[i] ?? [],
+            PNG: files.PNGFiles[i] ?? [],
+          },
+          compilerConfig,
+        },
+      });
+
+      // Once all work is done by the threads, clean up some stuff...
+      const promise = new Promise((resolve) => {
+        worker?.once("message", (result) => {
+          resolve(result);
+          void worker.terminate();
+        });
+      });
+
+      this.promises.push(promise);
+    }
   }
 
-  const requestQueue: {
-    fn: string;
-    resolve: (value: unknown) => void;
-    reject: unknown;
-    data?: unknown;
-  }[] = [];
-
-  const processQueue = () => {
-    const idleThread = threadPool.find((worker) => !worker.busy);
-    if (!idleThread || requestQueue.length === 0) return;
-
-    const { fn, resolve, data } = requestQueue.shift()!;
-    const worker = idleThread?.worker;
-    idleThread.busy = true;
-
-    const jsonString = JSON.stringify({ fn, data });
-    const { buffer } = encoder.encode(jsonString);
-
-    worker?.postMessage({ buffer }, [buffer]);
-
-    worker?.once("message", (result) => {
-      idleThread.busy = false;
-      resolve(result);
-      processQueue();
-    });
-  };
-
-  const runThread = (fn: FunctionNames, data: unknown) =>
-    new Promise((resolve, reject) => {
-      requestQueue.push({ fn, resolve, reject, data });
-      processQueue();
-    });
-
-  const run = (taskName: FunctionNames, fn: FunctionNames, data: unknown) =>
-    new Promise((resolve) => {
-      const runningText = `${upFirstChar(taskName)}...`;
-      const afterText = `Finished ${lowFirstChar(taskName)}.`;
-
-      const spinner = newSpinner(runningText);
-      void runThread(fn, data).then((data) => {
-        resolve(data);
-        spinner("success", { text: afterText });
-      });
-    });
-
-  const runArray = (fn: FunctionNames, array: unknown[], data: object) =>
-    Promise.all(array.map((element) => runThread(fn, { ...data, element })));
-
-  const terminatePool = () => {
-    for (let i = 0; i < threads; i++) {
-      void threadPool[i].worker.terminate();
-    }
-  };
-
-  return { terminatePool, runArray, run };
-};
+  async finalize() {
+    return (await Promise.all(this.promises)).flatMap((x) => x);
+  }
+}
--- src/workers/worker.ts
+++ src/workers/worker.ts
@@ -1,121 +1,101 @@
-import { readFileSync, existsSync, writeFileSync } from "node:fs";
-import { join } from "node:path";
-import { parentPort, workerData } from "node:worker_threads";
-import { createHash } from "node:crypto";
-import { errorListFile } from "../utils/compiler/temp_folder.js";
-import { copyFile } from "node:fs/promises";
-import type { JSONErrorList } from "../types/error_list.d.ts";
-import type { WorkerData, WorkerFunctions } from "../types/workers.d.ts";
 import sharp from "sharp";
-import { PNG } from "../utils/compiler/default_config.js";
-import { promiseAllUnhandled } from "../utils/compiler/promise_all_unhandled.js";
+import { join } from "path";
 import JSONC from "jsonc-parser";
+import { createHash } from "crypto";
+import { copyFile, writeFile } from "fs/promises";
+import { PNG } from "../utils/compiler/default_config.js";
+import { existsSync, readFileSync, writeFileSync } from "fs";
+import { workerData as _WD, parentPort } from "worker_threads";
 
-const { cache, temp, compilerConfig } = workerData as WorkerData;
+import { tempPack, cache } from "../utils/compiler/temp_folder.js";
+import { promiseAllUnhandled } from "../utils/compiler/promise_all_unhandled.js";
 
-// TODO: Reduce some repeated code
+import type { WorkerData } from "../types/workers.d.ts";
 
-const functions: WorkerFunctions = {
-  async copyEtc({ inPath, element: etc }) {
-    // If array is empty, ignore it right away
-    if (!etc[0]) return;
+// For proper type checking and to nullify some data...
+const {
+  compilerConfig: { compiler },
+  files,
+  inPath,
+} = _WD as WorkerData;
+const errorList: unknown[] = [];
 
-    const promises = etc.map((path) =>
-      copyFile(join(inPath, path), join(temp, path))
-    );
-
-    await promiseAllUnhandled(promises);
-  },
-  async minifyJSON({ inPath, element: JSONFiles }) {
-    // If array is empty, ignore it right away
-    if (!JSONFiles[0]) return;
-
-    const errorList: JSONErrorList = [];
-
-    const promises = JSONFiles.map((path) => {
-      const tempPath = join(temp, path);
-      const filePath = join(inPath, path);
-
-      let data = readFileSync(filePath, "utf-8");
-      const hash = createHash("md5").update(data).digest("hex");
-
-      const cacheFile = join(cache, hash.concat(".json"));
-      const cacheFileExists = existsSync(cacheFile);
-
-      if (!cacheFileExists) {
-        if (compilerConfig.JSON?.errorChecking) {
-          try {
-            data = JSONC.stripComments(data);
-            data = JSON.parse(data) as unknown as string; // epic bypass
-          } catch (error) {
-            errorList.push({
-              error: (error as Error).toString(),
-              filePath,
-            });
-          }
-        } else {
-          data = JSONC.parse(data) as unknown as string; // epic bypass 2: Electric Boogaloo
-        }
-
-        writeFileSync(cacheFile, JSON.stringify(data));
-      }
-
-      return copyFile(cacheFile, tempPath);
+const checkParseJSON = (data: string, filePath: string): unknown => {
+  try {
+    return JSON.parse(JSONC.stripComments(data));
+  } catch (error) {
+    errorList.push({
+      error: (error as Error).toString(),
+      filePath,
     });
-
-    await promiseAllUnhandled(promises);
-
-    if (!(compilerConfig.JSON?.errorChecking && errorList[0])) return;
-
-    if (!existsSync(errorListFile)) writeFileSync(errorListFile, "[]");
-
-    const allErrors = JSON.parse(
-      readFileSync(errorListFile, "utf-8")
-    ) as JSONErrorList;
-    allErrors.push(...errorList);
-
-    writeFileSync(errorListFile, JSON.stringify(allErrors));
-  },
-  async compressPNG({ inPath, element: PNGFiles }) {
-    // If array is empty, ignore it right away
-    if (!PNGFiles[0]) return;
-
-    const promises = PNGFiles.map(async (path) => {
-      const tempPath = join(temp, path);
-      const filePath = join(inPath, path);
-
-      const buffer = readFileSync(filePath);
-      const hash = createHash("md5").update(buffer).digest("hex");
-
-      const cacheFile = join(cache, hash.concat(".png"));
-      const cacheFileExists = existsSync(cacheFile);
-
-      if (!cacheFileExists) await sharp(filePath).png(PNG).toFile(cacheFile);
-
-      return copyFile(cacheFile, tempPath);
-    });
-
-    await promiseAllUnhandled(promises);
-  },
-  // async compressJPG({ inPath, element: PNGFiles }) {},
+    return false;
+  }
 };
 
-const decoder = new TextDecoder();
+function minifyJSON() {
+  if (files.JSON.length === 0) return;
+  const { errorChecking } = compiler.JSON ?? { errorChecking: false };
 
-const receiveEncodedJSON = (buffer: ArrayBuffer) => {
-  const jsonString = decoder.decode(buffer);
-  return JSON.parse(jsonString) as object | never[];
-};
+  const promises = files.JSON.map((path) => {
+    const tempPath = join(tempPack, path);
+    const filePath = join(inPath, path);
 
-// eslint-disable-next-line @typescript-eslint/no-misused-promises
-parentPort!.on("message", async ({ buffer }) => {
-  const { fn, data } = receiveEncodedJSON(buffer as ArrayBuffer) as {
-    fn: keyof WorkerFunctions;
-    data: never;
-  };
+    const data = readFileSync(filePath, "utf-8");
+    const hash = createHash("md5").update(data).digest("hex");
 
-  if (typeof functions[fn] === "undefined")
-    throw new Error("Function is not defined!");
+    const cacheFile = join(cache, hash.concat(".json"));
+    const cacheFileExists = existsSync(cacheFile);
 
-  parentPort?.postMessage(await functions[fn](data));
-});
+    if (!cacheFileExists) {
+      const parsedJSON: unknown = errorChecking
+        ? checkParseJSON(data, filePath)
+        : JSONC.parse(data);
+
+      if (!parsedJSON) return;
+
+      if (!compiler.withCaching)
+        return writeFile(tempPath, JSON.stringify(data));
+
+      writeFileSync(cacheFile, JSON.stringify(parsedJSON));
+    }
+
+    return copyFile(cacheFile, tempPath);
+  });
+
+  // @ts-expect-error Help out garbage collector
+  files.JSON = null;
+
+  return promiseAllUnhandled(promises);
+}
+
+function compressPNG() {
+  if (files.PNG.length === 0) return;
+
+  const promises = files.PNG.map(async (path) => {
+    const tempPath = join(tempPack, path);
+    const filePath = join(inPath, path);
+
+    const buffer = readFileSync(filePath);
+    const hash = createHash("md5").update(buffer).digest("hex");
+
+    const cacheFile = join(cache, hash.concat(".png"));
+    const cacheFileExists = existsSync(cacheFile);
+
+    if (!cacheFileExists) {
+      if (!compiler.withCaching)
+        return sharp(filePath).png(PNG).toFile(tempPath);
+
+      await sharp(filePath).png(PNG).toFile(cacheFile);
+    }
+
+    return copyFile(cacheFile, tempPath);
+  });
+
+  // @ts-expect-error Help out garbage collector
+  files.PNG = null;
+
+  return promiseAllUnhandled(promises);
+}
+
+void (await Promise.all([minifyJSON(), compressPNG()]));
+parentPort?.postMessage(errorList);
