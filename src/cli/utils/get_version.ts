0324b6c803fb733bc298c02ce0c24f88dafc0868
TBroz15 <78770366+TBroz15@users.noreply.github.com>
2024-10-23 16:19:32 +0800

    refactor(cli): :recycle: get package.json through a one liner

--- src/cli/utils/get_version.ts
+++ src/cli/utils/get_version.ts
@@ -1,10 +1,1 @@
-import { readFileSync } from "fs";
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-export const { version } = JSON.parse(
-  readFileSync(resolve(__dirname, "../../../package.json"), "utf-8")
-) as { version: string };
+export { version } from "../../../package.json" assert { type: "json" };
