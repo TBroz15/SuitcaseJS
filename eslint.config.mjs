1aae2fe22696ce6d0d03041ebab4af03f93585f8
TBroz15 <78770366+TBroz15@users.noreply.github.com>
2024-11-08 02:21:42 +0800

    chore: bruh

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,18 @@
 import eslint from "@eslint/js";
 import tselint from "typescript-eslint";
 
-export default tselint.config(
-  eslint.configs.recommended,
-  ...tselint.configs.recommendedTypeChecked,
-  {
-    ignores: [
-      "node_modules/**/*",
-      "*.mjs",
-      "ci/*.js",
-      "dist/**/*",
-      "src/compiler/utils/JSONC/*.js",
-    ],
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+export default tselint.config(eslint.configs.recommended, {
+  ignores: [
+    "node_modules/**/*",
+    "*.mjs",
+    "ci/*.js",
+    "dist/**/*",
+    "src/compiler/utils/JSONC/*.js",
+  ],
+  languageOptions: {
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
     },
-  }
-);
+  },
+});
